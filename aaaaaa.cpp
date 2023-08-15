9bcf96685c7893e264c94ac9e5bce1f5f8db1a75
juanmedinah <142339507+juanmedinah@users.noreply.github.com>
2023-08-15 11:21:09 -0600

    Add files via upload

---
+++ aaaaaa.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+int main ()
+{
+	system("color 1e");
+	setlocale(LC_ALL,"spanish");
+	printf("\t\t\tGENERACIONES DE COMPUTADORAS\n\n\t\tPrimera Generación (1940s - mediados de 1950s)\n\nCaracterísticas:\nUtilización de válvulas de vacío para el procesamiento.\nTamaño y peso considerable, ocupaban salas enteras.\nProgramación manual mediante conexiones de cables y switches.\n\n");
+	printf("\t\tSegunda Generación (Finales de 1950s - 1960s):\n\nCaracterísticas:\nReemplazo de las válvulas de vacío por transistores, lo que redujo el tamaño y el calor generado.\nUtilización de cintas magnéticas y tambores para almacenamiento.\nDesarrollo de lenguajes de programación de nivel mas alto, como el FORTRAN y el COBOL.\n\n\t\tTercera Generación (1960s - 1970s):\n\nCaracterísticas:\nUso de circuitos integrados (chips) que permitieron mayor velocidad y capacidad.\nSurgimiento de las computadoras mainframe y minicomputadoras.\nMayor disponibilidad de sistemas operativos y multiprogramación.\n\n\t\tCuarta Generación (1970s - 1980s):\n\nCaracterísticas:\nMicroprocesadores permitieron la creación de las computadoras personales (PCs).\nMayor integración en un solo chip, lo que aumento la eficiencia.\nDesarrollo de lenguajes de programación orientados a objetos como C++.\n\n\t\tQuinta Generación (1980s - presente):\n\nCaracterísticas:\nAvances en inteligencia artificial y procesamiento paralelo.\nDesarrollo de lenguajes de programación de alto nivel como Python.\nUso de tecnologías como la nanotecnología y la computacion cuántica.");
+}
